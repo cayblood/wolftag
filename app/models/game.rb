10760e6ba402e843264da37cf67442ec2f2e8d1f
Nicolai Berthelsen <nicobert@cisco.com>
2013-04-12 09:48:37 +0200

    added inverse_of: nil and removed foreign key as it is redundant

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,7 @@ class Game
   include Mongoid::Document
   field :_id, type: String, default: -> { name.to_s.parameterize }
   field :name, type: String
-  field :it_id, type: String
-  belongs_to :it, class_name: :player, foreign_key: :it_id
+  belongs_to :it, class_name: "Player", inverse_of: nil
   has_and_belongs_to_many :players
 
   validates_uniqueness_of :name
