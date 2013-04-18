610d8e4db75379c2171202d2a23abd9a59bda078
Carl Youngblood <carl@youngbloods.org>
2013-04-18 16:13:13 +0200

    Fixed id auto-generation in models

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,9 +1,16 @@
 class Game
   include Mongoid::Document
-  field :_id, type: String, default: -> { name.to_s.parameterize }
+  field :_id, type: String
   field :name, type: String
   belongs_to :it, class_name: "Player", inverse_of: nil
   has_and_belongs_to_many :players
 
   validates_uniqueness_of :name
+
+  before_create :create_id
+
+  private
+  def create_id
+   self.id = name.to_s.parameterize
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,8 +1,15 @@
 class Player
   include Mongoid::Document
-  field :_id, type: String, default: -> { name.to_s.parameterize }
+  field :_id, type: String
   field :name, type: String
   has_and_belongs_to_many :games
 
   validates_uniqueness_of :name
+
+  before_create :create_id
+
+  private
+  def create_id
+    self.id = name.to_s.parameterize
+  end
 end
